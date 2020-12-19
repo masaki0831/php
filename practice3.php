b909dc8ef04034d6d3a548dbf1031a9feba6845e
東口将己 <abc.hilcy@gmail.com>
2020-12-19 11:01:43 +0000

    php

---
+++ practice3.php
@@ -0,0 +1,25 @@
+<?php
+$hello ="Hello";
+$name ="Masaki";
+$world ="'s World!";
+echo $hello . $name . $world ;
+
+$tech_boost ="tech";
+$tech_boost .=' boost';
+echo $tech_boost;
+
+$calendar = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+echo $calendar["December"];
