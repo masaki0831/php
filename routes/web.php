92db9ac6a86ac14604497ef27140aac365661c35
東口将己 <abc.hilcy@gmail.com>
2021-01-04 09:04:06 +0000

    PHP/Laravel 13

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,14 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create');
 });
 
 Route::group(['prefix' => 'admin'], function(){
     Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
     Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+    Route::post('profile/create','Admin\ProfileController@create');
+    Route::post('profile/edit','Admin\ProfileController@update');
 });
 
 Auth::routes();
