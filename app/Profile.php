2aaa7f27eabce0cb5f639da9f123803809830588
東口将己 <abc.hilcy@gmail.com>
2021-01-16 09:56:19 +0000

    PHP/Laravel 17

--- app/Profile.php
+++ app/Profile.php
@@ -6,11 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    //
+    protected $guarded = array('id');
+
     public static $rules = array(
         'name' => 'required',
         'gender' => 'required',
         'hobby' => 'required',
         'introduction' => 'required',
     );
+    
+    public function profilehistories()
+    {
+      return $this->hasMany('App\Profilehistory');
+
+    }
 }
